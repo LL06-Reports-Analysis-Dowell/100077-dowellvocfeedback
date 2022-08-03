e37f5f63260b4f55a545f3d8584ff7378ac55f59
WawinyEdwin <wawinyedwin44@gmail.com>
2022-08-03 12:10:31 +0300

    Add content for other page template and setup views

--- feedback/urls.py
+++ feedback/urls.py
@@ -3,7 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('code/', views.code, name='code'),
-    path('emailqrcode/', views.emailqrcode, name='qrcode'),
-    
+    path('brand-details/', views.code, name='code'),
+    path('email-qr-code/', views.createQrCode, name='qrcode'),
+    path('email-confirmation/', views.confirm, name='confirmmail'),
+    path('preview/', views.preview, name='preview'),
+    path('help-video/', views.helpvideo, name="video"),
+    path('privacy-policy/', views.policy, name='policy'),
+    path('recommend-friend/', views.recommend, name='recommend'),
+    path('show-qr-code/', views.showqr, name='showqr'),
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -6,16 +6,41 @@ from django.shortcuts import redirect, render
 def home(request):
     return render(request, 'feedback/home.html', { })
 
+# Brand Details------------------------------------------------------
 def code(request):
     return render(request, 'feedback/code.html', { })
 
-def emailqrcode(request):
-    return render(request, 'feedback/emailqrcode.html',{})
+# Show QR Code ------------------------------------------------------
+def showqr(request):
+    return render(request, 'feedback/showqrcode.html',{})
+
+# Preview Page------------------------------------------------------
+def preview(request):
+    return render(request, 'feedback/preview.html',{})
+
+# Help Video------------------------------------------------------
+def helpvideo(request):
+    return render(request, 'feedback/help-video.html',{})
+
+# Privacy Policy------------------------------------------------------
+def policy(request):
+    return render(request, 'feedback/policy.html',{})
+
+# Recommend Friend------------------------------------------------------
+def recommend(request):
+    return render(request, 'feedback/recommendfriend.html',{})
+
+# 
+def confirm(request):
+    return render(request, 'feedback/emailconfirmation.html',{})
+
 
 # Handle Brand Details------------------------------------------------------
-def qrcode(request):
+def createQrCode(request):
     if request.method == 'POST':
         form = request.POST
         print(form)
         form.save()
-        return redirect('/feedback/emailqrcode.html', {})
+        return redirect('', {})
+    else:
+        return render(request, 'feedback/emailqrcode.html',{})
