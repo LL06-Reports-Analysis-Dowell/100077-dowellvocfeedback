688c51a9e89a71d0118d808a5328cd7d000ca1b0
WawinyEdwin <wawinyedwin44@gmail.com>
2022-08-05 12:24:05 +0300

    Add function to save brand details to database

--- feedback/models.py
+++ feedback/models.py
@@ -6,11 +6,11 @@ from django.db import models
 class Brand(models.Model):
     brand_name = models.CharField(max_length=100)
     brand_product_name = models.CharField(max_length=100)
-    brand_picture = models.FileField(upload_to='uploads/%Y/%m/%d/')
+    brand_logo = models.FileField(upload_to='uploads/%Y/%m/%d/')
     brand_qr_code_url = models.CharField(max_length=500)
     brand_qr_code_picture = models.FileField(upload_to='uploads/%Y/%m/%d/')
     brand_user_name = models.CharField(max_length=100)
-    brand_user_email = models.EmailField()
+    # brand_user_email = models.EmailField()
 
     def __str__(self):
         return self.brand_name
--- feedback/views.py
+++ feedback/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import redirect, render
 
+from feedback.models import Brand
+
 # Create your views here.
 
 # Home Page------------------------------------------------------
 def home(request):
     return render(request, 'feedback/home.html', { })
 
-# Brand Details------------------------------------------------------
-def code(request):
-    return render(request, 'feedback/code.html', { })
 
 # Show QR Code ------------------------------------------------------
 def showqr(request):
@@ -30,20 +29,27 @@ def policy(request):
 def recommend(request):
     return render(request, 'feedback/recommendfriend.html',{})
 
-# 
+# Confirm email------------------------------------------------------
 def confirm(request):
     return render(request, 'feedback/emailconfirmation.html',{})
+
+# Email Qr Code------------------------------------------------------
+def emailqr(request):
+    return render(request, 'feedback/emailqrcode.html',{})
 
 
 # Handle Brand Details------------------------------------------------------
 def createQrCode(request):
+    # print(request.POST)
     if request.method == 'POST':
-        form = request.POST
-        print(form)
-        form.save()
-        return redirect('feedback/showqrcode.html', {})
+        brand_logo = request.FILES['brand_picture']
+        brand_name = request.POST['brand_name']
+        brand_product_name = request.POST['brand_product_name']
+        new_brand = Brand(brand_logo=brand_logo, brand_name=brand_name, brand_product_name=brand_product_name)
+        new_brand.save()
+        return redirect('/show-qr-code/')
     else:
-        return render(request, 'feedback/emailqrcode.html',{})
+        return render(request, 'feedback/code.html',{})
 
 def error_404_view(request, exception):
     return render(request, 'feedback/404.html', {}, status=404)
