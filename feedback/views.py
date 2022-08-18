c0a425294fb8fd1fedf6ab52b8d2100ce5d237b0
WawinyEdwin <wawinyedwin44@gmail.com>
2022-08-18 14:58:34 +0300

    Add brand data encoding| decode from request to show on feedback

--- feedback/views.py
+++ feedback/views.py
@@ -4,9 +4,23 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.core.mail import send_mail
 from PIL import Image
+from cryptography.fernet import Fernet
 from feedback.qrcode_gen import qrgen
 from .models import Brand
 
+
+# Encode Function
+def encode(key,text):
+    cipher_suite = Fernet(key.encode())
+    encoded_text = cipher_suite.encrypt(text.encode())
+    return encoded_text
+
+def decode(key,decodetext):
+    cipher_suite = Fernet(key.encode())
+    decoded_text = cipher_suite.decrypt(decodetext.encode())
+    return decoded_text.decode()
+# Encryption Key------------------------------------------------------
+key="l6h8C92XGJmQ_aXpPN7_VUMzA8LS8Bg50A83KNcrVhQ="
 
 # Create your views here.
 def error_404_view(request, exception):
@@ -33,40 +47,52 @@ def helpvideo(request):
 def create_Qr_Code(request):
 
     context = {}
+    base_url = 'http://127.0.0.1:8000/'
 
     # Get Brand Data
     if request.method == 'POST':
-        brand_logo = request.FILES['brand_picture']
-        brand_name = request.POST['brand_name']
-        brand_product_name = request.POST['brand_product_name']
+        # Get from request ans encode it
+        brand_name_raw = request.POST['brand_name']
+        brand_name  = encode(key,brand_name_raw)
+        brand_product_name_raw = request.POST['brand_product_name']
+        brand_product_name = encode(key,brand_product_name_raw)
 
-        # Application Link
-        brand_qr_code_url =  f"http://127.0.0.1:8000/brandurl/?brand={brand_name}&product={brand_product_name}&logo={brand_logo}"
+        # Get Brand Image, Encode & Format name
+        brand_logo_raw = request.FILES['brand_picture']
+        brand_logo_formatted = brand_logo_raw.name.replace(" ","")
+        brand_logo = encode(key,brand_logo_formatted)
 
-        # Formart Brand Logo
-        brand_logo1 = brand_logo.name.replace(" ","")
+        # Application Links
+        brand_qr_code_url =  f"{base_url}brandurl/?brand={brand_name}&product={brand_product_name}&logo={brand_logo.decode()}"
+       
+        
 
         # create QR from brand data.
-        qrgen(brand_logo, brand_qr_code_url, brand_name, brand_product_name, f"media/qrcodes/{brand_logo1}",brand_logo)
+        qrgen(brand_logo_raw, f"{base_url}brandurl", brand_name.decode(), brand_product_name.decode(), f"media/qrcodes/{brand_logo_formatted}",brand_logo.decode())
 
         # Save Brand Data   
-        brand = Brand(brand_name=brand_name, brand_product_name=brand_product_name, brand_logo=brand_logo,  brand_qr_code_picture=f'media/qrcodes/{brand_logo1}', brand_qr_code_url=brand_qr_code_url)
+        brand = Brand(brand_name=brand_name, brand_product_name=brand_product_name, brand_logo=brand_logo_raw,  brand_qr_code_picture=f'media/qrcodes/{brand_logo_formatted}', brand_qr_code_url=brand_qr_code_url)
         brand.save()
 
         # save the diff thumbnail for the brand logo
-        with Image.open(f"media/qrcodes/{brand_logo1}") as image:
+        with Image.open(f"media/qrcodes/{brand_logo_formatted}") as image:
             image.thumbnail((128,128))
-            image.save(f"media/qrcodes/thumbnails/{brand_logo1}","JPEG")
-        with Image.open(f"media/brandlogos/{brand_logo.name.replace(' ','_')}") as image:
+            image.save(f"media/qrcodes/thumbnails/{brand_logo_formatted}","JPEG")
+        with Image.open(f"media/brandlogos/{brand_logo_raw.name.replace(' ','_')}") as image:
             image.thumbnail((256,256))
-            image.save(f"media/brandlogos/thumbnails/{brand_logo1}",quality=100)
+            image.save(f"media/brandlogos/thumbnails/{brand_logo_formatted}",quality=100)
+
+        
+
+        brand_qr_code_url = f"{base_url}brandurl?brand={brand_name.decode()}&product={brand_product_name.decode()}&logo={brand_logo.decode()}"
+        
+        context["brand_qr_code_url"] = brand_qr_code_url
+        context["brand_qr_code_picture"] = brand_logo_formatted
+        context["brand"] = brand_name_raw
+        context["product"] = brand_product_name_raw
+        context['base_url'] = base_url
 
         # return to Show QR page
-        context["brand_qr_code_url"] = brand_qr_code_url
-        context["brand_qr_code_picture"] = brand_logo1
-        context["brand"] = brand_name
-        context["product"] = brand_product_name
-
         return render(request, 'feedback/showqrcode.html' , context)
     return render(request, 'feedback/code.html')
 
@@ -140,19 +166,28 @@ def recommend(request):
 
 def feedback(request):
     context = {}
-    context['success'] = 'Thank you for your feedback.'
+    
+
+    # Brand Data & Rating
     if request.method == 'POST':
         rating = request.POST['rating']
-        brand_name = request.POST['brand_name']
+        brand_name_encoded = request.POST['brand_name']
         brand_product_name = request.POST['brand_product_name']
+
+        context['success'] = 'Thank you for your feedback.'
         return render(request, 'feedback/thankyou.html', context)
 
-    brand_name = request.GET.get('brand', None)
-    brand_product_name = request.GET.get('product', None)
-    brand_logo = request.GET.get('logo', None)
-    print(brand_product_name)
+    #  From the Request URL.
+    brand_name_encoded = request.GET.get('brand', None)
+    brand_name = decode(key, brand_name_encoded)
+    brand_product_name_encoded = request.GET.get('product', None)
+    brand_product_name = decode(key, brand_product_name_encoded)
+    brand_logo_encoded = request.GET.get('logo', None)
+    brand_logo = decode(key, brand_logo_encoded)
 
+    # Pass Brand Data to Template------------------------------------------------------
     context['brand_name'] = brand_name
     context['brand_product_name'] = brand_product_name
     context['brand_logo'] = brand_logo
+
     return render(request, 'feedback/feedback.html', context)
