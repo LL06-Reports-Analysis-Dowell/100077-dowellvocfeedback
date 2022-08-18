f5aa94b38deedfb5c4f6a2b0347cfc8443e71f69
WawinyEdwin <wawinyedwin44@gmail.com>
2022-08-18 15:12:01 +0300

    Add required pkg lib info

--- feedback/views.py
+++ feedback/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.conf import settings
-from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.core.mail import send_mail
 from PIL import Image
